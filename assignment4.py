4177c037f27cdc069ca02db95386b06f94a7ddae
ASHISH KUMAR <145495068+IamAshishSingh02@users.noreply.github.com>
2024-08-03 23:45:52 +0530

    assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -1,64 +1,76 @@
-def inputPoly(name):
-    degree=int(input("Degree of "+name+ ": "))
-    poly=[]
-    for i in range(degree+1):
-        coeff=float(input("Coefficient of x^" +str(i)+": "))
-        poly.append(coeff)
-    return poly
+#Input polnomials Func
+def input_poly(p):
+    print(p)
+    deg=int(input("\nEnter the degree of the polynomial: "))
+    coeffs=[]
+    for i in range(deg+1):
+        c=float(input(f"Enter the coefficient of x^{i}: "))
+        coeffs.append(c)
+    return coeffs
 
-def printPoly(poly,n):
-    for i in range(n-1,-1,-1):
-        if poly[i]!=0:
-            if poly[i]>0 and i!=n-1:
-                print(" + ",end="")
-            if poly[i]<0:
-                print(" - ",end="")
-            if abs(poly[i])!=1 or i==0:
-                print(abs(poly[i]),end="")
-            if i!=0:
-                print("x",end="")
-                if i != 1:
-                    print("^",i,end="")
-    print()
+#Printing Polynomials Func
+def print_poly(coeffs):
+    terms=[]
+    for i in range(len(coeffs)):
+        if coeffs[i]!=0:
+            term=""
+            if coeffs[i]!=1 or i==0:
+                term+=str(coeffs[i])
+            if i>0:
+                term+="x"
+                if i>1:
+                    term+="^"+str(i)
+            terms.append(term)
+    print("+".join(reversed(terms)).replace("+ -","- "))
 
-print("Enter coefficients of polynomial A")
-A=inputPoly("A")
-print("\nEnter coefficients of polynomial B")
-B=inputPoly("B")
+#Evaluating Polynomials Func
+def eval(coeffs,x):
+    result=0
+    for i in range(len(coeffs)):
+        result+=coeffs[i]*(x**i)
+    return result
 
-m=len(A)
-n=len(B)
+#Adding Polynomials Func
+def add(c1,c2):
+    max_len=max(len(c1),len(c2))
+    result=[0]*max_len
+    for i in range(max_len):
+        if i<len(c1):
+            result[i]+=c1[i]
+        if i<len(c2):
+            result[i]+=c2[i]
+    return result
 
-print("\nA is: ")
-printPoly(A,m)
-print("B is: ")
-printPoly(B,n)
+#Multiplying Polynomials Func
+def mul(c1,c2):
+    result=[0]*(len(c1)+len(c2)-1)
+    for i in range(len(c1)):
+        for j in range(len(c2)):
+            result[i+j]+=c1[i]*c2[j]
+    return result
 
-#Addition
-def add(A,B,m,n):
-    size=max(m,n)
-    sum=[0]*size
-    for i in range(m):
-        sum[i]=A[i]
-    for i in range(n):
-        sum[i]+=B[i]
-    return sum
+#Taking Input from User
+p1=input_poly("\nEnter the first polynomial")
+p2=input_poly("\nEnter the second polynomial")
 
-sum=add(A,B,m,n)
-size=max(m,n)
-print("\nSum of A & B is: ")
-printPoly(sum,size)
+#Printing Given Polynomials
+print("\nPolynomial I:")
+print_poly(p1)
+print("\nPolynomial II:")
+print_poly(p2)
 
-#Multipication
-def multiply(A,B):
-    m=len(A)
-    n=len(B)
-    product=[0]*(m+n-1)
-    for i in range(m):
-        for j in range(n):
-            product[i+j]+=A[i]*B[j]
-    return product
+#Evaluating & Printing Polynomials at Given Value of x
+x1=float(input("\nEnter the value of x to evaluate Polynomial I: "))
+x2=float(input("Enter the value of x to evaluate Polynomial II: "))
+print("\nPolynomial I evaluated at x=",x1,":",eval(p1,x1))
+print("Polynomial II evaluated at x=",x2,":",eval(p2,x2))
 
-product=multiply(A,B)
-print("Product of A & B is: ")
-printPoly(product,size)
+#Adding & Printing Polynomials
+p3=add(p1,p2)
+print("\n[Polynomial I]+[Polynomial II]:")
+print_poly(p3)
+
+#Multiplying & Printing Polynomials
+p4=mul(p1,p2)
+print("\n[Polynomial I]X[Polynomial II]:")
+print_poly(p4)
