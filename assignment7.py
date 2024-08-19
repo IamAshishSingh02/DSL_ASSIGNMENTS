74099068e24f31496f7aac7f31ae83352fe08fcc
ASHISH KUMAR <145495068+IamAshishSingh02@users.noreply.github.com>
2024-08-19 10:26:01 +0530

    assignment7.py

--- assignment7.py
+++ assignment7.py
@@ -24,7 +24,7 @@ def insertion_sort(arr):
     for i in range(1,n):
         key=arr[i]
         j=i-1
-        while j>=0 and key<arr[j]:
+        while(j>=0 and key<arr[j]):
             arr[j+1]=arr[j]
             j-=1
         arr[j+1]=key
@@ -33,16 +33,16 @@ def insertion_sort(arr):
 #Shell Sort
 def shell_sort(arr):
     n=len(arr)
-    gap=n//2
-    while gap>0:
-        for i in range(gap,n):
+    x=n//2
+    while(x>0):
+        for i in range(x,n):
             temp=arr[i]
             j=i
-            while j>=gap and arr[j-gap]>temp:
-                arr[j]=arr[j-gap]
-                j-=gap
+            while(j>=x and arr[j-x]>temp):
+                arr[j]=arr[j-x]
+                j-=x
             arr[j]=temp
-        gap//=2
+        x//=2
     return arr
 
 #Input
